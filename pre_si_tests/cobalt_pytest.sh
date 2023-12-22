9e149167925418847a8cf18c592f1ef342967840
taozha2 <tao2.zhang@intel.com>
2023-12-22 10:05:00 +0800

    align with public main branch

--- pre_si_tests/cobalt_pytest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-test_framework_path="$script_path/drivers_gpu_compute_workloads"
-
-PYTHONPATH="$test_framework_path" pytest "$@"
