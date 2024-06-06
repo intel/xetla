8cf02f3d6ca436a54c3112a4af6fd748f2dc376d
taozha2 <tao2.zhang@intel.com>
2024-06-05 22:59:21 -0700

    how to add test case

--- gemm_benchmark_bf16_bf16_float_float.sh
+++ gemm_benchmark_bf16_bf16_float_float.sh
@@ -1,6 +1,11 @@
+# how to add a case
+  # 1. add a new test case "class Test_xxx : public TestBase ... " in tests/integration/gemm/bf16/common.hpp
+  # 2. add the test name "Test_xxx" to tests/integration/gemm/bf16/main.cp
+
+# build and run
+  # ./gemm_benchmark_bf16_bf16_float_float.sh
+
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf $script_dir/build && mkdir $script_dir/build && cd $script_dir/build
 source ./../tools/scripts/env.sh
-cmake ..
-cd ./tests/integration/gemm/bf16
-make -j && ./gemm_bf16
+cmake .. && make gemm_bf16 && ./tests/integration/gemm/bf16/gemm_bf16
