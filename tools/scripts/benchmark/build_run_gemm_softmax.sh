7d9a7d1eda8c2bce897efe9b11dfdff0305168e1
taozha2 <tao2.zhang@intel.com>
2024-10-21 00:58:51 -0700

    update scripts

--- tools/scripts/benchmark/build_run_gemm_softmax.sh
+++ tools/scripts/benchmark/build_run_gemm_softmax.sh
@@ -1,12 +1,29 @@
-export SYCL_PROGRAM_COMPILE_OPTIONS=" -vc-codegen -vc-disable-indvars-opt -doubleGRF -Xfinalizer ' -printregusage -enableBCR -DPASTokenReduction ' "
+unset SYCL_PROGRAM_COMPILE_OPTIONS
+unset sycl_compiler_path
+unset gpu_driver_path
+unset without_softmax
+unset without_reduction
+
+export SYCL_PROGRAM_COMPILE_OPTIONS=" -vc-codegen -vc-disable-indvars-opt -Xfinalizer ' -printregusage -enableBCR -DPASTokenReduction ' "
+
+# https://github.com/intel/llvm/releases/tag/nightly-2024-05-16
+sycl_compiler_path=/opt/intel_validation/oneapi/compiler/latest/
+
+# https://ubit-gfx.intel.com/build/19168301/artifacts
+gpu_driver_path=/opt/cutlass/gpu_driver/gfx-driver-ci-comp_igc-27004/extract/
+
+export CPATH=$sycl_compiler_path/include:$sycl_compiler_path/include/sycl:/opt/intel_validation/oneapi/2024.0/include/
+export CC=${sycl_compiler_path}/bin/icx
+export CXX=${sycl_compiler_path}/bin/icpx
+
+export LIBRARY_PATH=$gpu_driver_path/usr/lib/x86_64-linux-gnu/:$sycl_compiler_path/lib/:/opt/intel_validation/oneapi/2024.0/lib/
+export LD_LIBRARY_PATH=$LIBRARY_PATH
+export MKLROOT=/opt/intel_validation/oneapi/mkl/2024.0/
 
 export IGC_ShaderDumpEnable=1
 export IGC_DumpToCustomDir=./xetla_dumps
 
-unset without_softmax
-unset without_reduction
-
-without_softmax=" -DWITHOUT_SOFTMAX "
+#without_softmax=" -DWITHOUT_SOFTMAX "
 #without_reduction=" -DWITHOUT_REDUCTION "
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
