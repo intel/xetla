44b1e9c69f3c4a1e539468e197f4dfb374332251
taozha2 <tao2.zhang@intel.com>
2024-11-12 22:08:01 -0800

    update

--- tools/scripts/benchmark/build_run_gemm_softmax.sh
+++ tools/scripts/benchmark/build_run_gemm_softmax.sh
@@ -17,8 +17,8 @@ sycl_compiler_path=/opt/intel/oneapi/compiler/2024.2
 export MKLROOT=/opt/intel/oneapi/mkl/2024.2/
 
 # https://ubit-gfx.intel.com/build/19168301/artifacts
-gpu_driver_path=/opt/cutlass/gpu_driver/agama-996.6/extract/
-#gpu_driver_path=/opt/cutlass/gpu_driver/gfx-driver-ci-comp_igc-27004/extract/
+#gpu_driver_path=/opt/cutlass/gpu_driver/agama-996.6/extract/
+gpu_driver_path=/opt/cutlass/gpu_driver/gfx-driver-ci-comp_igc-27004/extract/
 
 export CPATH=$sycl_compiler_path/include:$sycl_compiler_path/include/sycl:/opt/intel/oneapi/2024.2/include/
 export CC=$sycl_compiler_path/bin/icx
@@ -28,17 +28,21 @@ export LIBRARY_PATH=$gpu_driver_path/usr/lib/x86_64-linux-gnu/:$sycl_compiler_pa
 export LD_LIBRARY_PATH=$LIBRARY_PATH
 
 export IGC_ShaderDumpEnable=1
-export IGC_DumpToCustomDir=./xetla_dumps
+export IGC_DumpToCustomDir=$repo_path/build/xetla_dumps/
 
-#disable_prefetch=" -DDISABLE_GEMM_PREFETCH "
+disable_prefetch=" -DDISABLE_GEMM_PREFETCH "
 #without_softmax=" -DWITHOUT_SOFTMAX "
 #without_reduction=" -DWITHOUT_REDUCTION "
 
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 repo_path=$script_dir/../../../
-rm -rf $repo_path/build && mkdir $repo_path/build && cd $repo_path/build
+rm -rf $repo_path/build/* && cd $repo_path/build
 
 source $repo_path/tools/scripts/env_debug.sh
 cmake .. -DCMAKE_CXX_FLAGS=" $without_softmax $without_reduction $disable_prefetch " \
 && make gemm_softmax \
 && ./examples/06_gemm_softmax/gemm_softmax
+
+#/home/zt/workspace/cutlass/unitrace/tools/unitrace/build/unitrace --chrome-kernel-logging --stall-sampling -i 20 -o xetla_pvc_gemm_softmax.csv ./examples/06_gemm_softmax/gemm_softmax
+#python3 ~/workspace/cutlass/unitrace/tools/unitrace/scripts/analyzeperfmetrics.py -s $IGC_DumpToCustomDir -t "XVE Stalls by Instruction" $csv_file -o ${csv_file}.pdf
+
